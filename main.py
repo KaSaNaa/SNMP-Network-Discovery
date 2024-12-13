6eeb821ac5528d1562cfe5b3bdbbd999e71e6f5e
KaSaNaa <pasindudeshitha123@gmail.com>
2024-12-13 12:08:15 +0530

    minor changes

--- main.py
+++ main.py
@@ -1,21 +1,38 @@
 import json
-from utils.network_utils import NetworkUtils
-from utils.snmp_manager import SNMPManager
-from utils.graph_manager import GraphManager
+from core.network_utils import NetworkUtils
+from core.snmp_manager import SNMPManager
+from core.graph_manager import GraphManager
+from core.utils import ensure_directory_exists
 
 # Example usage
 if __name__ == "__main__":
-    pass # Comment this line to run the code block below
+    # Comment this line to run the code block below
+    snmp_manager = SNMPManager(version=2, community='public')
+    
+    output_file_path = 'data/discovered_devices.json'
+    
+    ensure_directory_exists(output_file_path)
+    
     # ----------------------------------------------------------------
     # * Uncomment the following code block to perform recursive discovery on a single IP
     
-    # output_file_path = 'data/discovered_devices.json'
-    # snmp_manager = SNMPManager(version=2, community='public')
-    
-    # ip = '192.168.62.20'
-    # # "1.3.6.1.2.1.2.2.1.2"    "1.3.6.1.4.1.9.9.23.1.2.1.1.4"
-    # result = snmp_manager.recursive_discovery(ip)
-    # with open(output_file_path, 'w') as json_file:
-    #     json.dump(result, json_file, indent=4)
+    ip = '192.168.63.11'
+    result = snmp_manager.recursive_discovery(ip)
+    with open(output_file_path, 'w') as json_file:
+          json.dump(result, json_file, indent=4)
     
     # ----------------------------------------------------------------
+    # * Uncomment the following code block to perform recursive discovery on a subnet
+    
+#     subnet = ['192.168.62.0/24']
+#     ip_list = NetworkUtils.get_ips_from_subnets(subnet)
+    
+#     active_ips = NetworkUtils.scan_subnet(ip_list)
+    
+#     result = []
+    
+#     for ip in active_ips:
+#          result.append(snmp_manager.recursive_discovery(ip))
+    
+#     with open(output_file_path, 'w') as json_file:
+#          json.dump(result, json_file, indent=4)        
